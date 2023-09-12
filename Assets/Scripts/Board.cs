70301bd5789e95f2fe83ab368ae53f9fc0561727
zekiyedogr <72526615+zekiyedogr@users.noreply.github.com>
2023-09-13 02:06:04 +0300

    Gems Moving

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [Header("deneme")]
+    public int width;
+    public int height;
+
+    public GameObject bgTilePrefab;
+}
